c70ea8766e51f11c239ecac4be4c58e3b9d0e76d
Michael Menzel <menzel@hush.com>
2012-07-27 10:10:59 +0200

    helper added

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,13 @@
 module RecipesHelper
+
+  def get_voting recipe
+    if recipe.vote_count == nil || recipe.vote_count == 0;
+      score = 0
+    else
+      score = @recipe.vote_sum / @recipe.vote_count
+    end
+    score
+  end
+
+
 end
